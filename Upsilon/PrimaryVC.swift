6f97070a06cf950e6d6f855fce7abae5e73305c8
Pavel Seleznev <pavel.seleznev@kolabnow.ch>
2019-05-07 05:43:28 +0300

    Four tabs added

---
+++ Upsilon/PrimaryVC.swift
@@ -0,0 +1,16 @@
+//
+//  PrimaryVC.swift
+//  Upsilon
+//
+//  Created by Pavel Seleznev on 4/25/19.
+//  Copyright © 2019 Pavel Seleznev. All rights reserved.
+//
+
+import UIKit
+
+class PrimaryVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
