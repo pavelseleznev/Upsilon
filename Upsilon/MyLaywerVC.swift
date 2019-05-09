cdd7fa01bbd625f10dd33c572f9fca52efc4eec9
Pavel Seleznev <pavel.seleznev@kolabnow.ch>
2019-05-09 11:12:55 +0300

    Implemented MyLaywerVC

---
+++ Upsilon/MyLaywerVC.swift
@@ -0,0 +1,16 @@
+//
+//  MyLaywerVC.swift
+//  Upsilon
+//
+//  Created by Pavel Seleznev on 5/9/19.
+//  Copyright © 2019 Pavel Seleznev. All rights reserved.
+//
+
+import UIKit
+
+class MyLaywerVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
