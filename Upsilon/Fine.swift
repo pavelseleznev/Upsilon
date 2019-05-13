5547488874a48bd55b35263241b03bd375cee996
Pavel Seleznev <pavel.seleznev@kolabnow.ch>
2019-05-13 12:37:04 +0300

    Implemented Fines list

---
+++ Upsilon/Fine.swift
@@ -0,0 +1,14 @@
+//
+//  Fine.swift
+//  Upsilon
+//
+//  Created by Pavel Seleznev on 5/13/19.
+//  Copyright © 2019 Pavel Seleznev. All rights reserved.
+//
+
+import Foundation
+
+struct Fine {
+    var fineDescription: String
+    var fineAmount: String
+}
